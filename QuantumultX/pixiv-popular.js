69e30a22582e4a546b342d55612b06b73316cd84
Koi <maoxia19840912@qq.com>
2022-10-11 22:43:41 +0800

    merge pixiv.js; fix pixiv.js;

--- QuantumultX/pixiv-popular.js
+++
@@ -1,4 +0,0 @@
-var modifiedHeaders = $request.headers;
-var modifiedPath = $request.path.replace(/search\/illust(.+)search_target=(partial|exact)/,"search/popular-preview/illust$1search_target=exact");
-console.log(modifiedPath);
-$done({path : modifiedPath , headers : modifiedHeaders});
--- QuantumultX/pixiv.js
+++ QuantumultX/pixiv.js
@@ -1,14 +1,24 @@
+const $ = API("Pixiv", false);
 const body = JSON.parse($response.body);
-if ($request && $request.url.indexOf('/auth/token') >= 0) {
-  const Koi = init();
-  const token = body.access_token;
-  body.user['is_premium'] = true;
-  body.response.user['is_premium'] = true;
-  if (token) Koi.setdata(token, 'Koi_signtoken_pixiv');
-} else if ($request && $request.url.indexOf('user/detail') > -1) {
-  body.profile['is_premium'] = true;
-} else {
-  body = $response.body
+const path = $request.path;
+
+  if (path.indexOf("/v1/user/detail") != -1) {
+    body.profile['is_premium'] = true;
+  }
+  if (path.indexOf("auth/token") != -1) {
+      body.user['is_premium'] = true;
+      body.response.user['is_premium'] = true;
+  }
+  if (path.indexOf("/touch/ajax/user/self/status") != -1) {
+        body["show_ads"] = false;
+        body["user_premium"] = 1;
+        body["is_premium"] = ture;
+  }
+  if (path.indexOf("/touch/ajax_api/ajax_api\.php") != -1) {
+          body["user_data"]["user_premium"] = 1;
+  }
+  if (path.indexOf("php?mode=r18") != -1) {
+    body = $response.body
     .replace(/"user_premium":"\d+/, '"user_premium":"1')
     .replace(/"is_premium":false/, '"is_premium":true')
     .replace(/"ads_disabled":false/, '"ads_disabled":true')
@@ -20,49 +30,20 @@ if ($request && $request.url.indexOf('/auth/token') >= 0) {
     )
     .replace(/"pixiv.config.ad":true/, '"pixiv.config.ad":false')
     .replace(/"pixiv.strings.nopremium":".*?"/, '');
-  $done({ body: body });
-}
-$done({ body: JSON.stringify(body) });
-function init() {
-  isSurge = () => {
-    return undefined === this.$httpClient ? false : true;
-  };
-  isQuanX = () => {
-    return undefined === this.$task ? false : true;
-  };
-  getdata = key => {
-    if (isSurge()) return $persistentStore.read(key);
-    if (isQuanX()) return $prefs.valueForKey(key);
-  };
-  setdata = (key, val) => {
-    if (isSurge()) return $persistentStore.write(key, val);
-    if (isQuanX()) return $prefs.setValueForKey(key, val);
-  };
-  msg = (title, subtitle, body) => {
-    if (isSurge()) $notification.post(title, subtitle, body);
-    if (isQuanX()) $notify(title, subtitle, body);
-  };
-  log = message => console.log(message);
-  get = (url, cb) => {
-    if (isSurge()) {
-      $httpClient.get(url, cb);
-    }
-    if (isQuanX()) {
-      url.method = 'GET';
-      $task.fetch(url).then(resp => cb(null, resp, resp.body));
-    }
-  };
-  post = (url, cb) => {
-    if (isSurge()) {
-      $httpClient.post(url, cb);
-    }
-    if (isQuanX()) {
-      url.method = 'POST';
-      $task.fetch(url).then(resp => cb(null, resp, resp.body));
-    }
-  };
-  done = (value = {}) => {
-    $done(value);
-  };
-  return { isSurge, isQuanX, msg, log, getdata, setdata, get, post, done };
-}
+    $.done({ body: body });
+  }
+  if (path.indexOf("/v1/search/illust") != -1) {
+    let modifiedHeaders = $request.headers;
+    let modifiedPath = $request.path.replace(/search\/illust(.+)search_target=(partial|exact)/,"search/popular-preview/illust$1search_target=exact");
+    console.log(modifiedPath);
+    $.done({path : modifiedPath , headers : modifiedHeaders});
+  }
+  $.done({ body: JSON.stringify(body) });
+
+
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+//https://raw.githubusercontent.com/Peng-YM/QuanX/master/Tools/OpenAPI
+function ENV(){const e="function"==typeof require&&"undefined"!=typeof $jsbox;return{isQX:"undefined"!=typeof $task,isLoon:"undefined"!=typeof $loon,isSurge:"undefined"!=typeof $httpClient&&"undefined"!=typeof $utils,isBrowser:"undefined"!=typeof document,isNode:"function"==typeof require&&!e,isJSBox:e,isRequest:"undefined"!=typeof $request,isScriptable:"undefined"!=typeof importModule}}function HTTP(e={baseURL:""}){const{isQX:t,isLoon:s,isSurge:o,isScriptable:n,isNode:i,isBrowser:r}=ENV(),u=/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/;const a={};return["GET","POST","PUT","DELETE","HEAD","OPTIONS","PATCH"].forEach(h=>a[h.toLowerCase()]=(a=>(function(a,h){h="string"==typeof h?{url:h}:h;const d=e.baseURL;d&&!u.test(h.url||"")&&(h.url=d?d+h.url:h.url),h.body&&h.headers&&!h.headers["Content-Type"]&&(h.headers["Content-Type"]="application/x-www-form-urlencoded");const l=(h={...e,...h}).timeout,c={onRequest:()=>{},onResponse:e=>e,onTimeout:()=>{},...h.events};let f,p;if(c.onRequest(a,h),t)f=$task.fetch({method:a,...h});else if(s||o||i)f=new Promise((e,t)=>{(i?require("request"):$httpClient)[a.toLowerCase()](h,(s,o,n)=>{s?t(s):e({statusCode:o.status||o.statusCode,headers:o.headers,body:n})})});else if(n){const e=new Request(h.url);e.method=a,e.headers=h.headers,e.body=h.body,f=new Promise((t,s)=>{e.loadString().then(s=>{t({statusCode:e.response.statusCode,headers:e.response.headers,body:s})}).catch(e=>s(e))})}else r&&(f=new Promise((e,t)=>{fetch(h.url,{method:a,headers:h.headers,body:h.body}).then(e=>e.json()).then(t=>e({statusCode:t.status,headers:t.headers,body:t.data})).catch(t)}));const y=l?new Promise((e,t)=>{p=setTimeout(()=>(c.onTimeout(),t(`${a} URL: ${h.url} exceeds the timeout ${l} ms`)),l)}):null;return(y?Promise.race([y,f]).then(e=>(clearTimeout(p),e)):f).then(e=>c.onResponse(e))})(h,a))),a}function API(e="untitled",t=!1){const{isQX:s,isLoon:o,isSurge:n,isNode:i,isJSBox:r,isScriptable:u}=ENV();return new class{constructor(e,t){this.name=e,this.debug=t,this.http=HTTP(),this.env=ENV(),this.node=(()=>{if(i){return{fs:require("fs")}}return null})(),this.initCache();Promise.prototype.delay=function(e){return this.then(function(t){return((e,t)=>new Promise(function(s){setTimeout(s.bind(null,t),e)}))(e,t)})}}initCache(){if(s&&(this.cache=JSON.parse($prefs.valueForKey(this.name)||"{}")),(o||n)&&(this.cache=JSON.parse($persistentStore.read(this.name)||"{}")),i){let e="root.json";this.node.fs.existsSync(e)||this.node.fs.writeFileSync(e,JSON.stringify({}),{flag:"wx"},e=>console.log(e)),this.root={},e=`${this.name}.json`,this.node.fs.existsSync(e)?this.cache=JSON.parse(this.node.fs.readFileSync(`${this.name}.json`)):(this.node.fs.writeFileSync(e,JSON.stringify({}),{flag:"wx"},e=>console.log(e)),this.cache={})}}persistCache(){const e=JSON.stringify(this.cache,null,2);s&&$prefs.setValueForKey(e,this.name),(o||n)&&$persistentStore.write(e,this.name),i&&(this.node.fs.writeFileSync(`${this.name}.json`,e,{flag:"w"},e=>console.log(e)),this.node.fs.writeFileSync("root.json",JSON.stringify(this.root,null,2),{flag:"w"},e=>console.log(e)))}write(e,t){if(this.log(`SET ${t}`),-1!==t.indexOf("#")){if(t=t.substr(1),n||o)return $persistentStore.write(e,t);if(s)return $prefs.setValueForKey(e,t);i&&(this.root[t]=e)}else this.cache[t]=e;this.persistCache()}read(e){return this.log(`READ ${e}`),-1===e.indexOf("#")?this.cache[e]:(e=e.substr(1),n||o?$persistentStore.read(e):s?$prefs.valueForKey(e):i?this.root[e]:void 0)}delete(e){if(this.log(`DELETE ${e}`),-1!==e.indexOf("#")){if(e=e.substr(1),n||o)return $persistentStore.write(null,e);if(s)return $prefs.removeValueForKey(e);i&&delete this.root[e]}else delete this.cache[e];this.persistCache()}notify(e,t="",a="",h={}){const d=h["open-url"],l=h["media-url"];if(s&&$notify(e,t,a,h),n&&$notification.post(e,t,a+`${l?"\n多媒体:"+l:""}`,{url:d}),o){let s={};d&&(s.openUrl=d),l&&(s.mediaUrl=l),"{}"===JSON.stringify(s)?$notification.post(e,t,a):$notification.post(e,t,a,s)}if(i||u){const s=a+(d?`\n点击跳转: ${d}`:"")+(l?`\n多媒体: ${l}`:"");if(r){require("push").schedule({title:e,body:(t?t+"\n":"")+s})}else console.log(`${e}\n${t}\n${s}\n\n`)}}log(e){this.debug&&console.log(`[${this.name}] LOG: ${this.stringify(e)}`)}info(e){console.log(`[${this.name}] INFO: ${this.stringify(e)}`)}error(e){console.log(`[${this.name}] ERROR: ${this.stringify(e)}`)}wait(e){return new Promise(t=>setTimeout(t,e))}done(e={}){s||o||n?$done(e):i&&!r&&"undefined"!=typeof $context&&($context.headers=e.headers,$context.statusCode=e.statusCode,$context.body=e.body)}stringify(e){if("string"==typeof e||e instanceof String)return e;try{return JSON.stringify(e,null,2)}catch(e){return"[object Object]"}}}(e,t)}
