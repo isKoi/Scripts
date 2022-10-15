a8f8f2fbad4108517fd92585063763ffa7249020
Koi <maoxia19840912@qq.com>
2022-10-15 22:45:38 +0800

    update

--- QuantumultX/pixiv.js
+++ QuantumultX/pixiv.js
@@ -3,23 +3,29 @@ const $ = API('Pixiv', false);
 (async () => {
     let path = $request.path;
     path = path.slice(0, !path.indexOf('?') ? path.indexOf('?') : path.length);
+    $.log(path);
+    let body = null;
     let obj = $response ? JSON.parse($response.body) : null;
     switch (path) {
-        case 'auth/token':
+        case '/auth/token':
             obj['user']['is_premium'] = true;
             obj['response']['user']['is_premium'] = true;
+            body = JSON.stringify(obj);
             break;
         case '/v1/user/detail':
             obj['profile']['is_premium'] = true;
+            body = JSON.stringify(obj);
             break;
         case '/touch/ajax/user/self/status':
             obj = obj['boby']['user_status'];
             obj['ads_disabled'] = true;
             obj['show_ads'] = false;
             obj['user_premium'] = 1;
+            body = JSON.stringify(obj);
             break;
         case '/touch/ajax_api/ajax_api.php':
             obj['user_data']['user_premium'] = 1;
+            body = JSON.stringify(obj);
             break;
         case '/v1/search/illust':
             let modifiedHeaders = $request.headers;
@@ -34,7 +40,6 @@ const $ = API('Pixiv', false);
 })()
     .catch((e) => $.error(e.message || e.error || e))
     .finally(() => {
-        let body = JSON.stringify(obj);
         $.log(body);
         $.done({ body: body });
     });
